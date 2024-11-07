3ff26fb85bdb2ce02160a77491fe7c7d4718789b
judebacon18 <144885492+judebacon18@users.noreply.github.com>
2024-11-07 00:00:11 -0800

    Add files via upload

---
+++ jude.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	\<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>judde</title>
+
+	<h1>My first Ubuntu server PHP Deployment<h1>
+<form>
+
+  <label for="fname">First Name:</label>
+  <input type="text" id="fname" name="fname" required><br><br>
+
+  <label for="mname">Middle Name:</label>
+  <input type="text" id="mname" name="mname"><br><br>
+
+  <label for="lname">Last Name:</label>
+  <input type="text" id="lname" name="lname" required><br><br>
+
+  <label for="age">Age:</label>
+  <input type="number" id="age" name="age" required min="1"><br><br>
+
+  <label for="address">Address:</label>
+  <input type="text" id="address" name="address" required><br><br>
+
+  <label for="section">Section:</label>
+  <input type="text" id="section" name="section" required><br><br>
+
+  <input type="submit" value="Submit">
+</form>
+
+
+
+</head>
+<body>
+
+</body>
+</html>
